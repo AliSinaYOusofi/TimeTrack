8e772159bfd30dbd1c8ff07a0c3c1b2ba867ce66
Ali Sina Yousofi <92437693+AliSinaYOusofi@users.noreply.github.com>
2023-05-12 06:49:09 +0000

    working perfectly

---
+++ scripts/content-script.js
@@ -0,0 +1,263 @@
+let timeTrackContainer = document.createElement("div");
+let currentLocation = window.location.href;
+
+timeTrackContainer.innerHTML = `
+    <p class="timer"> </p>
+
+    <div class="icon_container">
+        <span class="minimize-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+            </svg>
+    
+        </span>
+
+        <span class='theme '>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+            </svg>
+        </span>
+    </div>
+`;
+
+timeTrackContainer.classList.add("time-track");
+
+let timeTrackText = timeTrackContainer.querySelector("p");
+let hideUnhideElements = timeTrackContainer.querySelector(".minimize-icon");
+let theme = timeTrackContainer.querySelector(".theme")
+
+let startPosX = 0;
+let startPosY = 0;
+let offsetX = 0;
+let offsetY = 0;
+
+document.body.appendChild(timeTrackContainer);
+
+let timerInterval;
+
+let startTime = Date.now();
+
+function incrementTime() {
+    
+    let currentTime = Date.now();
+    let timeDiff = currentTime - startTime;
+    let timeSeconds = Math.floor(timeDiff / 1000);
+    let timeString = formatTime(timeSeconds);
+    updateTimeDisplay(timeString);
+}
+
+function formatTime(time) {
+    
+    let seconds = time % 60;
+    let minutes = Math.floor(time / 60) % 60;
+    let hours = Math.floor(time / 3600);
+
+    seconds = (seconds < 10 ? "0" : "") + seconds;
+    minutes = (minutes < 10 ? "0" : "") + minutes;
+
+    return hours > 0 ? hours + ":" + minutes + ":" + seconds : minutes + ":" + seconds;
+}
+
+function resetTimer() {
+    
+    clearInterval(timerInterval);
+    startTime = Date.now();
+    updateTimeDisplay("00:00");
+    timerInterval = setInterval(incrementTime, 1000);
+
+}
+
+function handleLocationChange() {
+    
+    if (window.location.href !== currentLocation) {
+        resetTimer();
+        currentLocation = window.location.href;
+    }
+
+}
+
+timerInterval = setInterval(incrementTime, 1000);
+
+function updateTimeDisplay(timeSpent) {
+    
+    const styles = `
+    .time-track {
+        position: fixed;
+        z-index: 9999;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        padding: 12px;
+        background-color: #cc0066;
+        color: white;
+        border-radius: 10px;
+        font-size: 16px;
+        font-weight: bold;
+        text-align: center;
+        line-height: 1;
+        white-space: nowrap;
+        width: fit-content;
+        cursor: grab;
+        display: flex;
+        flex-flow: column wrap;
+        align-items: center;
+        justify-content: center;
+        gap: 10px;
+        transition: transform 0.2s ease-in-out;
+        background: black;
+        backdrop-filter: blur(10px);
+        opacity: 0.85;
+    }
+      
+    .time-track:hover {
+        transform: translate(-50%, -50%) scale(1.05);
+    }
+      
+    .reading-time-badge:hover {
+        transform: scale(1.1);
+    }
+      
+    .light {
+        background: white;
+        color: black;
+    }
+      
+    .dark {
+        background: black;
+        color: white;
+    }
+
+    .icon_container {
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: center;
+        align-items: center;
+        gap: 14px;
+      }
+    
+    .icon_container span {
+        margin-top: 10px;
+    }
+
+    .timer {
+        margin-top: -17px;
+        padding: 4px;
+    }
+    `;
+
+    const styleTag = document.createElement("style");
+
+    styleTag.textContent = styles;
+    document.head.insertAdjacentElement("beforeend", styleTag);
+    
+    timeTrackText.textContent = timeSpent;
+    
+    // resetTimerIcon.style.cursor = 'pointer';
+    hideUnhideElements.style.cursor = 'pointer';
+    theme.style.cursor = 'pointer';
+
+    // resetTimerIcon.style.width  = '25px';
+    hideUnhideElements.style.width  = '25px';
+    theme.style.width  = '25px';
+    timeTrackText.style.fontSize = '100px';
+
+    timeTrackContainer.classList.add("time-track");
+    
+    timeTrackContainer.addEventListener("mousedown", startDrag);
+    // resetTimerIcon.addEventListener("click", resetTimerOnClick);
+    theme.addEventListener("click", changeThemeOnClick);
+    hideUnhideElements.addEventListener("click", handleHideUnhideOnClick);
+
+}
+
+function saveTimeSpent() {
+    
+    let timeSpent = timeTrackText.textContent;
+    let currentTime = new Date().toLocaleString();
+    
+    chrome.runtime.sendMessage({ action: "saveTimeSpent", data: { timeSpent: timeSpent, currentTime: currentTime }}, function(response) {
+      console.log(response, 'afg');
+    });
+}
+
+function handleHideUnhideOnClick() {
+    
+    if (!hideUnhideElements.classList.contains("eye")) {
+        
+        hideUnhideElements.classList.add("eye");
+        
+        hideUnhideElements.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+        </svg>`
+        
+        // resetTimerIcon.style.display = 'none';
+        theme.style.display = 'none';
+        timeTrackText.style.display = 'none'
+
+    } else {
+        
+        hideUnhideElements.classList.remove("eye"); // remove the "eye" class
+        
+        hideUnhideElements.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
+        </svg>
+      `
+        // resetTimerIcon.style.display = 'flex';
+        theme.style.display = 'flex';
+        timeTrackText.style.display = 'flex'
+    }
+}
+
+function changeThemeOnClick() {
+    
+    if (timeTrackContainer.classList.contains("light")) {
+        timeTrackContainer.classList.remove("light");
+        timeTrackContainer.classList.add("dark");
+        theme.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" /></svg>`
+    } else {
+        timeTrackContainer.classList.remove("dark");
+        timeTrackContainer.classList.add("light");
+        theme.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" /></svg>`
+    }
+}
+
+
+// functions for dragging the div.
+function handleMouseDown(event) {
+    event.preventDefault();
+
+    dragStartX = event.clientX;
+    dragStartY = event.clientY;
+    const { top, left } = timeTrackContainer.getBoundingClientRect();
+    offsetX = dragStartX - left;
+    offsetY = dragStartY - top;
+}
+
+function moveElement(event) {
+    const newX = event.clientX - offsetX;
+    const newY = event.clientY - offsetY;
+
+    timeTrackContainer.style.left = newX + "px";
+    timeTrackContainer.style.top = newY + "px";
+}
+
+function startDrag(event) {
+    startPosX = timeTrackContainer.offsetLeft;
+    startPosY = timeTrackContainer.offsetTop;
+    offsetX = event.clientX - startPosX;
+    offsetY = event.clientY - startPosY;
+
+    document.addEventListener("mousemove", moveElement);
+}
+
+function stopDrag() {
+    document.removeEventListener("mousemove", moveElement);
+}
+
+document.addEventListener("mouseup", stopDrag);
+
+setInterval( () => {
+    saveTimeSpent()
+}, 1000)
---
+++ scripts/popup.js
@@ -0,0 +1,163 @@
+chrome.storage.local.get(null, function (data) {
+    
+    const sortedData = Object.values(data)
+        .sort((a, b) => new Date(a.currentTime).getTime() - new Date(b.currentTime).getTime())
+        .reverse();
+    
+    
+    let domains = {};
+
+    sortedData.forEach((item) => {
+
+        const website = item.currentLocation;
+        const domain = website?.split('/')[2];
+        
+        if (!domains[domain]) {
+            domains[domain] = [];
+        }
+        
+        domains[domain].push({
+            key: item.key,
+            website,
+            timeSpent: item.timeSpent,
+            currentTime: item.currentTime
+        });
+    });
+
+    for (const domain in domains) {
+        
+        const domainContainer = document.createElement("details");
+        
+        domainContainer.setAttribute("id", domain);
+        
+        const domainSummary = document.createElement("summary");
+        domainSummary.textContent = `${domain}  ${getTotalTime(domains[domain])}`;
+        domainSummary.style.fontWeight = "bold";
+        domainSummary.style.minWidth     = '400px'     
+        
+        const div = document.createElement("div");
+        div.style.marginLeft = '20px'
+
+        domains[domain].forEach(({website, timeSpent, currentTime}) => {
+            
+            const listItem = document.createElement("div");
+
+            listItem.classList.add("lists");
+            listItem.setAttribute("id", website);
+            listItem.style.display = 'flex';
+            listItem.style.justifyContent = 'space-between'; // center horizontally
+
+            const leftContainer = document.createElement("div");
+            leftContainer.style.display = "flex";
+            leftContainer.style.alignItems = "center";
+            leftContainer.style.justifyContent = 'space-evenly'
+
+            // create location section
+            const websiteLink = document.createElement("a");
+            websiteLink.setAttribute("href", website);
+            websiteLink.setAttribute("target", "_blank");
+            websiteLink.setAttribute("class", 'video-link');
+            websiteLink.textContent = website?.length > 40 ? website.slice(0, 40) + '...' : website;
+            websiteLink.style.marginLeft = '7px';
+            websiteLink.style.fontSize = '14px';
+            websiteLink.style.whiteSpace = 'nowrap';
+            websiteLink.style.textOverflow = 'ellipsis'; // Add this line to show an ellipsis
+            websiteLink.style.overflow = 'hidden'; // Add this line to hide the overflowing text
+            websiteLink.style.fontWeight = "bold";
+            websiteLink.style.cursor = 'pointer';
+
+            // create svg icon element
+            const icon = document.createElement("span");
+            icon.setAttribute("class", "delete_icon");
+            icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+          `;
+          // create time section
+            const time = document.createElement("p");
+            const spentTimeAt = document.createElement("span");
+
+            time.textContent = "Amount of time: " + " " + timeSpent;
+            spentTimeAt.textContent = 'Spent at: ' + currentTime;
+            time.setAttribute("class", 'date-time');
+            spentTimeAt.setAttribute("class", 'date-time');
+
+            // add click event to icon element to delete the li
+            icon.addEventListener("click", function() {
+                chrome.storage.local.remove(website, function() {
+                    listItem.remove();
+                    
+                    if (domains[domain].length === 0) {
+                        domainContainer.remove();
+                    }
+                    
+                    domainSummary.textContent = `Domain: ${domain} (${getTotalTime(domains[domain])})`;
+                });
+            });
+
+            // add location and icon sections to left container
+            leftContainer.appendChild(icon);
+            leftContainer.appendChild(websiteLink);
+            
+            // add time section to right container
+            listItem.appendChild(leftContainer);            
+            listItem.appendChild(time);
+            listItem.appendChild(spentTimeAt);
+            
+            
+            // append div to containerDiv
+            div.appendChild(listItem);
+            div.style.width = '350px'
+        });
+
+        // append list items to domain container
+        domainContainer.appendChild(domainSummary);
+        domainContainer.appendChild(div);
+
+        // append domain container to containerDiv
+        containerDiv.appendChild(domainContainer);
+    }
+});
+
+function getTotalTime(data) {
+    const total = data.reduce((acc, curr) => {
+        
+        const time = curr.timeSpent?.split(':');
+        
+        let seconds = 0;
+        
+        
+        if (time?.length === 3) {
+            seconds = (+time[0]) * 3600 + (+time[1]) * 60 + (+time[2]);
+        } else if (time?.length === 2) {
+            seconds = (+time[0]) * 60 + (+time[1]);
+        }
+    
+        return acc + seconds;
+    }, 0);
+  
+    const sec = total % 60;
+    const min = Math.floor(total / 60) % 60;
+    const hr = Math.floor(total / 3600);
+  
+    return `${hr.toString().padStart(2, '0')}:${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}`;
+}
+
+let searchInput = document.getElementById("search");
+
+searchInput.addEventListener("input", event => {
+
+    let searchTerm = event.target.value.toLowerCase();
+    let listItems = document.getElementsByClassName("lists");
+
+    for (let i = 0; i < listItems.length; i++) {
+        let domainContainer = listItems[i].closest("details");
+        let domainName = domainContainer.getAttribute("id").toLowerCase();
+
+        if (domainName.includes(searchTerm)) {
+            domainContainer.style.display = "";
+        } else {
+            domainContainer.style.display = "none";
+        }
+    }
+});
